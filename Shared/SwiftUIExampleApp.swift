342a32894bda417465a795917371410941d8d6fa
Dinakar Maurya <dinkar1708@gmail.com>
2021-01-05 16:09:46 +0900

    Initial Commit

---
+++ Shared/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  Shared
+//
+//  Created by Dinakar Maurya on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
